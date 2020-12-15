dacbce79962ddad5fe5498c894fc00ed2881b738
KojBot <KojBot@users.noreply.github.com>
2020-12-15 20:05:28 +0000

    [create-pull-request] automated change

--- src/comments.ts
+++ src/comments.ts
@@ -1,3 +1,9 @@
+/**
+ * This is an example file header
+ * @author Anand Chowdhary <mail@anandchowdhary.com>
+ * @link https://github.com/koj-co/header-comments
+*/
+
 export const commentTypes: {
   extensions: string[];
   firstLine?: string;
